58c46e9ad42e5befd19e2ecaad10f68b3febaae9
Carlosmape <cagrmape@gmail.com>
2022-04-02 20:52:44 +0200

    Improved GUI box sizing + added last commit remaining files

--- samples/cli_enhanced/gui_process.py
+++ samples/cli_enhanced/gui_process.py
@@ -1,7 +1,7 @@
 import multiprocessing
 from multiprocessing.queues import Queue
-from engine.characters.PlayerCharacter import PlayerCharacter
 from engine.frame import Frame
+from engine.world.area_types import area_types
 
 from samples.cli_enhanced.area_box import AreaBox
 from samples.cli_enhanced.command_line_box import CommandLineBox
@@ -25,7 +25,7 @@ class gui_process(multiprocessing.Process):
         # Calculate frame sizes for each part
         self.area_container = AreaBox(self.width, 2*self.height/3, self.scale_width, self.scale_height)
         self.status_container = PjStatsBox(self.width, self.height/4)
-        self.menu_container = MenuBox(self.width, self.height/8)
+        self.menu_container = MenuBox(self.width, self.height/12)
         self.isStarted = False
 
         self.frame_queue: Queue[Frame] = multiprocessing.Queue()
@@ -56,21 +56,30 @@ class gui_process(multiprocessing.Process):
                   
                 # Get Menu
                 str_gui += self.menu_container.render(self.frame.menu)
+
+                #Debug
+                str_gui += self.debug(self.frame)
     
-                remain_size = int(self.height - str_gui.count("\n")-2)
+                remain_size = int(self.height - str_gui.count("\n")-1)
                 #Print and reset cursor to console top-left
                 cursor_top = ""#'\033[0;0H'
-                print(cursor_top + str_gui+("\n"*remain_size) + self.debug(self.frame.player), end="\r")
+                print(cursor_top + str_gui+("\n"*remain_size), end="\r")
 
-    def debug(self, pj: PlayerCharacter):
-        str_dbg = style.CITALIC + style.CYELLOW
-        str_dbg += "Q full " + str(self.frame_queue.full()) + " size " + str(self.frame_queue.qsize())
-        if pj: 
-            str_dbg += " pos "+ str(pj.position) 
-            if (pj.is_moving):
-                str_dbg += " d %.3f" % (pj.last_distance) 
-                str_dbg += " t %.3f" % (pj.delta_time) 
-                str_dbg += " s %.3f" % (pj.last_distance / pj.delta_time if pj.last_distance and pj.delta_time else 0) 
-        return str_dbg + style.CEND
+    def debug(self, frame:Frame):
+        str_dbg = str()
+        if frame:
+            str_dbg = style.CITALIC + style.CYELLOW
+            str_dbg += "FrameQueue: " + str(self.frame_queue.qsize()) + " "
+            if frame.player: 
+                pj = frame.player
+                str_dbg += "PJ: "+ str(pj.position) 
+                if (pj.is_moving):
+                    str_dbg += " d %.3f" % (pj.last_distance) 
+                    str_dbg += " t %.3f" % (pj.delta_time) 
+                    str_dbg += " s %.3f" % (pj.last_distance / pj.delta_time if pj.last_distance and pj.delta_time else 0) 
+            if frame.area:
+                a = frame.area
+                str_dbg += " Area: " + area_types.NAMES[a.type] + " " + str(a.width) + "x" + str(a.height)
+        return str_dbg + style.CEND + "\n"
 
 
