6fcc81c22c0568e0f888cf8b12ff22a54788188f
Carlosmape <cagrmape@gmail.com>
2022-04-06 13:51:40 +0200

    [GUI-sample] Now Loading-box is alive. Improved animal models

--- samples/cli_enhanced/loading_box.py
+++ samples/cli_enhanced/loading_box.py
@@ -1,8 +1,8 @@
-from random import randint
-from engine.characters.Base import DIRECTION_EAST, DIRECTION_WEST, DIRECTIONS
+from engine.characters.AnimalCharacter import AnimalCharacter
 from engine.characters.NoPlayerCharacter import NoPlayerCharacter
 from engine.defines.defines import BodyParts, Position
 from engine.items.interactives.WearableItem import HandsWearable
+from engine.world.area import area
 from samples.cli_enhanced.area_box import AreaBox
 from samples.cli_enhanced.command_line_box import CommandLineBox
 
@@ -16,42 +16,44 @@ class LoadingBox(AreaBox):
         self.map_width = self.max_obj_in_width 
         self.map_height = self.max_obj_in_height
         self.tittle_box = CommandLineBox(width, height/5)
+        self.area = area(1)
         self.npcs = []
-        for i in range(4):
-            self.npcs.append(NoPlayerCharacter(None, 1, Position(self.middle_in_height,self.middle_in_width)))
-            self.npcs[i].items[BodyParts.hands] = HandsWearable.Any()
+        for i in range(int(self.max_objects_in_area/10)):
+            if i % 3 == 0:
+                self.npcs.append(AnimalCharacter(None, 1, self.area.generate_empty_position(False)))
+            else:
+                self.npcs.append(NoPlayerCharacter(None, 1, self.area.generate_empty_position(False)))
+                self.npcs[i].items[BodyParts.hands] = HandsWearable.Any()
+            self.npcs[i].area = self.area
 
     def _update_npcs(self):
         for npc in self.npcs:
-            action = randint(0,50)
-            if action == 0:
-                npc.is_moving = not npc.is_moving
-            elif action == 1:
-                npc.last_direction = DIRECTIONS[DIRECTION_WEST]
-            elif action == 2:
-                npc.last_direction = DIRECTIONS[DIRECTION_EAST]
-            elif action == 3:
-                npc.is_moving = False
+            npc.Move()
 
     def _extract_objects(self):
         self._update_npcs()
+
         items = []
         middle_height = int(self.max_obj_in_height/2)
         middle_width = int(self.max_obj_in_width/2)
+
         for y in range(0, self.max_obj_in_height):
             for x in range(0, self.max_obj_in_width):
+
+                # Draw ground by default
+                it = self.render_engine.render_ground(x*y)
+
+                # Or corresponding element
                 if y == middle_height and x == middle_width:
-                    items.append(self.render_engine.render_tittle(self.loaded))
-                elif y-2 == middle_height and x-2 == middle_width:
-                    items.append(self.render_engine.render_character(self.npcs[0]))
-                elif y+2 == middle_height and x-2 == middle_width:
-                    items.append(self.render_engine.render_character(self.npcs[1]))
-                elif y-2 == middle_height and x+2 == middle_width:
-                    items.append(self.render_engine.render_character(self.npcs[2]))
-                elif y+2 == middle_height and x+2 == middle_width:
-                    items.append(self.render_engine.render_character(self.npcs[3]))               
+                    it = self.render_engine.render_tittle(self.loaded)
+                elif Position(x,y) in self.area.items:
+                    it = self.render_engine.render_item(self.area.item(Position(x,y)))
                 else:
-                    items.append(self.render_engine.render_ground(x*y))
+                    for npc in self.npcs:
+                        if Position(x, y) == npc.position:
+                            it = self.render_engine.render_character(npc)
+
+                items.append(it)
 
         return items
    
--- samples/cli_enhanced/render/animal_render.py
+++ samples/cli_enhanced/render/animal_render.py
@@ -3,15 +3,44 @@ from .base_render import base_render
 
 
 class animal_render(base_render):
-    
+
+    head = ["º", "º", "º"]
+    torso = ["n","m", "n"]
+    tail = ["´", "'", "`"]
+    MAX_STEPS =  min(len(head), len(torso), len(tail))
     def __init__(self, background, foreground, item_bg = style.CBLACKBG):
-        super().__init__(7,3, background, foreground)
+        super().__init__(7,3, background, foreground, 0.5, animal_render.MAX_STEPS, True)
         self._item_bg = item_bg
-    
+        self._to_east = True
+        self._running = False
+        self._attacking = False
+
+    def update_state(self,to_east,running,attacking):
+        # Update state of the character, should be call just before render new frame
+        self._to_east = to_east
+        self._running = running
+        self._attacking = attacking
+
     def render(self):
-        composed_wall = []
-        composed_wall.append(self._back_color+self._fore_color+"       "+style.CEND)
-        composed_wall.append(self._back_color+self._fore_color+"  ºm'  "+style.CEND)
-        composed_wall.append(self._back_color+self._fore_color+"       "+style.CEND)
-        return composed_wall
+        composed_animal = []
+
+        curr_step = self._get_curr_step()
+        self._update_step()
+
+        body = self.compose_animal(curr_step)
+
+        composed_animal.append(self._back_color+self._fore_color+"       "+style.CEND)
+        composed_animal.append(self._back_color+self._fore_color+"  "+body+"  "+style.CEND)
+        composed_animal.append(self._back_color+self._fore_color+"       "+style.CEND)
+        return composed_animal
+    
+    def compose_animal(self, step:int):
+        head = animal_render.head[step]
+        tors = animal_render.torso[step]
+        tail = animal_render.tail[step]
+
+        if self._to_east:
+            return tail + tors + head
+        else:
+            return head + tors + tail
 
