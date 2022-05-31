6dd9afbeb5038da5190ac8ef7736f9c3f84428fc
Carlosmape <cagrmape@gmail.com>
2022-05-31 14:31:25 +0200

    Added some new sounds to the game

--- samples/cli_enhanced/ui/sound_process.py
+++ samples/cli_enhanced/ui/sound_process.py
@@ -1,4 +1,5 @@
 from engine.frame import Frame
+from .sounds.game_sounds import game_sounds
 from .sounds.char_sounds import char_sounds
 from .sounds.env_sounds import env_sounds
 
@@ -7,12 +8,14 @@ class sound_process():
         super().__init__()
 
         # Game
+        self.game_sounds = game_sounds()
         # Environment:
         self.env_sounds = env_sounds()
         # Player actions:
         self.char_sounds = char_sounds()
 
     def render(self, frame: Frame): 
+        self.game_sounds.update(frame)
         if frame.area:
             self.env_sounds.update(frame.area)
         if frame.player:
