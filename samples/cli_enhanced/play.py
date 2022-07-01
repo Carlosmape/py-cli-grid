cfd334bbbb80fbece8891338053bbbdced1f62d3
= <=>
2022-07-01 13:10:53 +0200

    Added new sub EdibleItemActions to allow them to interact with altered states (now eat edible specializes in eat beneficial/harmful edible items)

--- samples/cli_enhanced/play.py
+++ samples/cli_enhanced/play.py
@@ -13,22 +13,23 @@ from interface import CommandLineInterface
 from samples.assets.lore import characters, factions, cities, dialogs
 
 
-#########
-# Run Engine
-# Interface should be changed
-#########
-interface = CommandLineInterface()
-engine = Engine(
-    interface,
-    FactionRepository(factions.NAMES, factions.DESCRIPTIONS, factions.SLOGANS),
-    CityRepository(cities.NAMES),
-    CharacterRepository(characters.NAMES),
-    DialogRepository(
-        DialogElements(dialogs.SUSPICIOUS_GREETINGS, dialogs.NEUTRAL_GREETINGS, dialogs.FRIENDLY_GREETINGS),
-        DialogElements(dialogs.SUSPICIOUS_GOODBYES, dialogs.NEUTRAL_GOODBYES, dialogs.FRIENDLY_GOODBYES),
-        DialogElements(dialogs.SUSPICIOUS_TALK, dialogs.NEUTRAL_TALK, dialogs.NEUTRAL_TALK),
-        dialogs.GENERIC_CONFIRMATIONS,
-        dialogs.GAME_TIPS
+if __name__ == '__main__':
+    #########
+    # Run Engine
+    # Interface should be changed
+    #########
+    interface = CommandLineInterface()
+    engine = Engine(
+        interface,
+        FactionRepository(factions.NAMES, factions.DESCRIPTIONS, factions.SLOGANS),
+        CityRepository(cities.NAMES),
+        CharacterRepository(characters.NAMES),
+        DialogRepository(
+            DialogElements(dialogs.SUSPICIOUS_GREETINGS, dialogs.NEUTRAL_GREETINGS, dialogs.FRIENDLY_GREETINGS),
+            DialogElements(dialogs.SUSPICIOUS_GOODBYES, dialogs.NEUTRAL_GOODBYES, dialogs.FRIENDLY_GOODBYES),
+            DialogElements(dialogs.SUSPICIOUS_TALK, dialogs.NEUTRAL_TALK, dialogs.NEUTRAL_TALK),
+            dialogs.GENERIC_CONFIRMATIONS,
+            dialogs.GAME_TIPS
+        )
     )
-)
-engine.run()
+    engine.run()
