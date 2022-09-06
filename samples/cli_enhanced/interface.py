6498181cb4704f91687872044976907988c86afc
Carlosmape <cagrmape@gmail.com>
2022-09-06 20:10:42 +0200

    Increased base speed on debug

--- samples/cli_enhanced/interface.py
+++ samples/cli_enhanced/interface.py
@@ -29,6 +29,7 @@ class CommandLineInterface(GUI):
             Config.GameGuide.enabled = False
             Config.Area.min_height = Config.Area.max_height = int(self.height/3)
             Config.Area.min_width = Config.Area.max_width = int(self.width/7)
+            Config.Stats.movement_speed = 2
 
         # Initialize keyboard
         self.keyboard = KBHit()
