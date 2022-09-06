fa2c7aab24d2d06bf842dc4bc271774bc72582bd
Carlosmape <cagrmape@gmail.com>
2022-09-06 12:16:09 +0200

    Improving animal render animations

--- render/animal_render.py
+++ render/animal_render.py
@@ -4,13 +4,16 @@ from .base_render import base_render
 
 class animal_render(base_render):
 
-    head = ["º", "º", "º"]
+    #       ºn'  ºm´  ºn~
+    head = ["º", "º", "¤"]
     torso = ["n", "m", "n"]
-    tail = ["´", "'", "`"]
+    tail = ["'", "´", "´"]
     MAX_STEPS = min(len(head), len(torso), len(tail))
 
     def __init__(self, background, foreground, item_bg=style.CBLACKBG):
-        super().__init__(7, 3, background, foreground, 0.5, animal_render.MAX_STEPS, True)
+        super().__init__(7, 3, background, foreground, 1, animal_render.MAX_STEPS, True)
+        self._alt_anim = base_render(7, 3, background, foreground, 0.7, animal_render.MAX_STEPS, True)
+        self._act_anim = base_render(7, 3, background, foreground, 0.2, animal_render.MAX_STEPS, True)
         self._item_bg = item_bg
         self._to_east = True
         self._running = False
@@ -27,44 +30,52 @@ class animal_render(base_render):
         self._dead = dead
 
     def render(self):
-        if self._dead:
-            return self.compose_dead()
-        else:
-            return self.compose_alive()
+        return self.compose_alive()
 
     def compose_alive(self):
         composed_animal = []
 
-        curr_step = self._get_curr_step()
-        if not self._dead:
-            self._update_step()
-
-        body = self.compose_animal(curr_step)
-        composed_animal.append(self._colorize("       "))
-        composed_animal.append(self._colorize("  "+body+"  "))
-        composed_animal.append(self._colorize("       "))
-
-        if self._beingattacked:
-            for i in range(0, self._frame_height):
-                composed_animal[i] = style.CBOLD + composed_animal[i]
+        head_step = torso_step = self._get_curr_step()
+        tail_step = self._alt_anim._get_curr_step()
+        if self._running or self._attacking:
+            torso_step = tail_step = self._act_anim._get_curr_step()
         if self._attacking:
-            for i in range(0, self._frame_height):
-                composed_animal[i] = style.CITALIC + composed_animal[i]
-        return composed_animal
+            head_step = self._act_anim._get_curr_step()
 
-    def compose_dead(self):
-        composed_animal = []
-        curr_step = self._get_curr_step()
-        body = self.compose_animal(curr_step)
+        body = self.compose_animal(head_step, torso_step, tail_step)
         composed_animal.append(self._colorize("       "))
         composed_animal.append(self._colorize("  "+body+"  "))
         composed_animal.append(self._colorize("       "))
+
+        if not self._dead:
+
+            self._update_step()
+            self._alt_anim._update_step()
+            self._act_anim._update_step()
+
+            if self._beingattacked:
+                for i in range(0, self._frame_height):
+                    composed_animal[i] = style.CBOLD + composed_animal[i]
+            if self._attacking:
+                for i in range(0, self._frame_height):
+                    composed_animal[i] = style.CITALIC + composed_animal[i]
         return composed_animal
 
-    def compose_animal(self, step: int):
-        head = animal_render.head[step]
-        tors = animal_render.torso[step]
-        tail = animal_render.tail[step]
+    # def compose_dead(self):
+    #     composed_animal = []
+    #     curr_step = self._get_curr_step()
+    #     body = self.compose_animal(curr_step)
+    #     composed_animal.append(self._colorize("       "))
+    #     composed_animal.append(self._colorize("  "+body+"  "))
+    #     composed_animal.append(self._colorize("       "))
+    #     return composed_animal
+
+    def compose_animal(self, headstep: int, torsostep: int, tailstep: int):
+        head = animal_render.head[headstep]
+        if self._dead:
+            head = "•"
+        tors = animal_render.torso[torsostep]
+        tail = animal_render.tail[tailstep]
 
         if self._to_east:
             return tail + tors + head
