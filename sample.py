32106dc62d165e11044797fe9b883ce2eb3c33b6
Carlosmape <cagrmape@gmail.com>
2024-10-26 14:14:59 +0200

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,16 @@
+import os
+from command_line_box import CommandLineBox
+
+size = os.get_terminal_size()
+print("Current terminal size:")
+print(f" - Width:{size.columns}")
+print(f" - Height: {size.lines}")
+screen = CommandLineBox(size.columns, size.lines-6)
+sample_box = CommandLineBox(int(size.columns/2), size.lines-6)
+
+print(screen.render_cols([
+        sample_box.render("I am\nA text\nOn\nColumn"),\
+        sample_box.render("I am\nAnother text\nOn\nAnother Column"),\
+    ], False)
+)
+
